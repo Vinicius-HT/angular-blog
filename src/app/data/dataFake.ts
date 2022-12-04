b10941de96f2b9f7176ebebffe56f19fa706ad5a
Vinicius-HT <vinicius.h.trindade@gmail.com>
2022-12-04 14:57:36 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO DOUTOR ESTRANHO ANUNCIADO',
+    description:
+      'Marvel anuncia o novo filme do Doutor Estranho, com CGIs cada vez piores e um plot que faz menos sentido ainda!',
+    photoCover:
+      'https://legadodamarvel.com.br/wp-content/uploads/2022/12/Vaza-o-fim-dramatico-do-Kang-contra-os-herois-em-Vingadores-5-legadodamarvel.webp',
+  },
+  {
+    id: '2',
+    title: 'Atualizações sobre o novo filme do Deadpool!',
+    description:
+      'Novo filme do Deadpool vem para arrebentar com o retorno do Wolverine.',
+    photoCover:
+      'https://media.ambito.com/p/16b8e9f544467e8728738e9c96f6c863/adjuntos/239/imagenes/040/212/0040212110/deadpoolwebp.png',
+  },
+  {
+    id: '3',
+    title: 'Nova série da Marvel saindo do forno',
+    description:
+      'Mais uma série que não faz sentido, só para tirar mais um pouquinho de leite da franquia',
+    photoCover:
+      'https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2022/07/12/1806179816-marvel-studios.jpg',
+  },
+  {
+    id: '4',
+    title: 'Novidades sobre o novo lançamento da A24',
+    description:
+      'Chega aos cartazes em fevereiro de 2023 o mais novo filme da produtora A24 com seu tom sombrio e ar cult.',
+    photoCover:
+      'https://variety.com/wp-content/uploads/2022/08/5-Split-Offset-1.jpg?w=681&h=383&crop=1',
+  },
+];
